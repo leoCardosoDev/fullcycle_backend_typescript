10fdf00869c6a56ffa51f378c041fbcb33d8b913
leoCardosoDev <leocardosodev@gmail.com>
2023-12-21 08:58:55 +0000

    feat: add Repository Interface and CategoryRepositoryInterface

--- src/category/domain/category_repository.ts
+++ src/category/domain/category_repository.ts
@@ -1,1 +1,5 @@
-export interface CategoryRepository {}
+import { IRepository } from '../../shared/domain/repository/repository_interface'
+import { Uuid } from '../../shared/domain/value_objects/uuid_vo'
+import { Category } from './category_entity'
+
+export interface ICategoryRepository extends IRepository<Category, Uuid> {}
