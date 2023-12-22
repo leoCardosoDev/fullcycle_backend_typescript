357ef6d0bacfa70d99306fd8bfd27d69fa2a5245
leoCardosoDev <leocardosodev@gmail.com>
2023-12-22 10:54:49 +0000

    feat: ensure CategoryInMemoryRepository implements InMemorySearchableRepository

--- src/category/infra/db/category_in_memory_repository.ts
+++ src/category/infra/db/category_in_memory_repository.ts
@@ -1,8 +1,19 @@
-import { InMemoryRepository } from '../../../shared/infra/db/in_memory/in_memory_repository'
+import { InMemorySearchableRepository } from '../../../shared/infra/db/in_memory/in_memory_repository'
 import { Uuid } from '../../../shared/domain/value_objects/uuid_vo'
 import { Category } from '../../domain/category_entity'
+import { SortDirection } from '../../../shared/domain/repository/search_params'
 
-export class CategoryInMemoryRepository extends InMemoryRepository<Category, Uuid> {
+export class CategoryInMemoryRepository extends InMemorySearchableRepository<Category, Uuid> {
+  sortableFields: string[] = ['name', 'created_at']
+  protected async applyFilter(items: Category[], filter: string): Promise<Category[]> {
+    if (!filter) {
+      return items
+    }
+    return items.filter(i => i.name.toLowerCase().includes(filter.toLowerCase()))
+  }
+  protected applySort (items: Category[], sort: string | null, sort_dir: SortDirection | null) {
+    return sort ? super.applySort(items, sort, sort_dir) : super.applySort(items, 'created_at', 'desc')
+  }
   getEntity(): new (...args: any[]) => Category {
     return Category
   }
