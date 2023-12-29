4bdd4fffe1d9a70d452b2fc77ea7aab2027b4396
leoCardosoDev <leocardosodev@gmail.com>
2023-12-29 10:28:47 +0000

    test: add mapping props

--- src/category/infra/db/sequelize/__tests__/category_model_integration.spec.ts
+++ src/category/infra/db/sequelize/__tests__/category_model_integration.spec.ts
@@ -1,4 +1,4 @@
-import { Sequelize } from 'sequelize-typescript'
+import { DataType, Sequelize } from 'sequelize-typescript'
 import { CategoryModel } from '../category_model'
 import { Category } from '../../../../domain/category_entity'
 
@@ -14,6 +14,59 @@ describe('CategoryModel Integration Tests', () => {
     })
     await sequelize.sync({ force: true })
   })
+
+  test("mapping props", () => {
+    const attributesMap = CategoryModel.getAttributes();
+    const attributes = Object.keys(CategoryModel.getAttributes());
+
+    expect(attributes).toStrictEqual([
+      "category_id",
+      "name",
+      "description",
+      "is_active",
+      "created_at",
+    ]);
+
+    const categoryIdAttr = attributesMap.category_id;
+    expect(categoryIdAttr).toMatchObject({
+      field: "category_id",
+      fieldName: "category_id",
+      primaryKey: true,
+      type: DataType.UUID(),
+    });
+
+    const nameAttr = attributesMap.name;
+    expect(nameAttr).toMatchObject({
+      field: "name",
+      fieldName: "name",
+      allowNull: false,
+      type: DataType.STRING(255),
+    });
+
+    const descriptionAttr = attributesMap.description;
+    expect(descriptionAttr).toMatchObject({
+      field: "description",
+      fieldName: "description",
+      allowNull: true,
+      type: DataType.TEXT(),
+    });
+
+    const isActiveAttr = attributesMap.is_active;
+    expect(isActiveAttr).toMatchObject({
+      field: "is_active",
+      fieldName: "is_active",
+      allowNull: false,
+      type: DataType.BOOLEAN(),
+    });
+
+    const createdAtAttr = attributesMap.created_at;
+    expect(createdAtAttr).toMatchObject({
+      field: "created_at",
+      fieldName: "created_at",
+      allowNull: false,
+      type: DataType.DATE(3),
+    });
+  });
 
   test('should create a category', async () => {
     const category = Category.fake().aCategory().build()
