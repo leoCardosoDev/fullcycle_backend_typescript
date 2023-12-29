731a932ecb79b1cfc5407a878b07b79520e75281
leoCardosoDev <leocardosodev@gmail.com>
2023-12-29 10:58:01 +0000

    refactor: change extends to CategoryRepository

--- src/category/domain/category_repository.ts
+++ src/category/domain/category_repository.ts
@@ -1,5 +1,5 @@
-import { IRepository } from '../../shared/domain/repository/repository_interface'
+import { IRepository, ISearchableRepository } from '../../shared/domain/repository/repository_interface'
 import { Uuid } from '../../shared/domain/value_objects/uuid_vo'
 import { Category } from './category_entity'
 
-export interface ICategoryRepository extends IRepository<Category, Uuid> {}
+export interface ICategoryRepository extends ISearchableRepository<Category, Uuid> {}
