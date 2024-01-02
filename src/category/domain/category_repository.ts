ed3061a34c79c7ec001a953923b1c507492297ed
leoCardosoDev <leocardosodev@gmail.com>
2024-01-02 10:46:47 +0000

    feat: add CategorySearchParams and CategorySearchReasult

--- src/category/domain/category_repository.ts
+++ src/category/domain/category_repository.ts
@@ -1,5 +1,13 @@
-import { IRepository, ISearchableRepository } from '../../shared/domain/repository/repository_interface'
+import { ISearchableRepository } from '../../shared/domain/repository/repository_interface'
+import { SearchParams } from '../../shared/domain/repository/search_params'
+import { SearchResult } from '../../shared/domain/repository/search_result'
 import { Uuid } from '../../shared/domain/value_objects/uuid_vo'
 import { Category } from './category_entity'
 
-export interface ICategoryRepository extends ISearchableRepository<Category, Uuid> {}
+export type CategoryFilter = string
+
+export class CategorySearchParams extends SearchParams<CategoryFilter> {}
+
+export class CategorySearchResult extends SearchResult<Category> {}
+
+export interface ICategoryRepository extends ISearchableRepository<Category, Uuid, CategoryFilter, CategorySearchParams, CategorySearchResult> {}
