adfba677ca8630846fe0caa156477c6c25a60a6d
leoCardosoDev <leocardosodev@gmail.com>
2024-01-02 10:46:56 +0000

    feat: add CategorySearchParams and CategorySearchReasult

--- src/category/infra/db/in_memory/category_in_memory_repository.ts
+++ src/category/infra/db/in_memory/category_in_memory_repository.ts
@@ -2,10 +2,11 @@ import { InMemorySearchableRepository } from '../../../../shared/infra/db/in_mem
 import { Uuid } from '../../../../shared/domain/value_objects/uuid_vo'
 import { Category } from '../../../domain/category_entity'
 import { SortDirection } from '../../../../shared/domain/repository/search_params'
+import { CategoryFilter, ICategoryRepository } from '../../../domain/category_repository'
 
-export class CategoryInMemoryRepository extends InMemorySearchableRepository<Category, Uuid> {
+export class CategoryInMemoryRepository extends InMemorySearchableRepository<Category, Uuid> implements ICategoryRepository {
   sortableFields: string[] = ['name', 'created_at']
-  protected async applyFilter(items: Category[], filter: string): Promise<Category[]> {
+  protected async applyFilter(items: Category[], filter: CategoryFilter): Promise<Category[]> {
     if (!filter) {
       return items
     }
